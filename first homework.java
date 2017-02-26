26cc101882fcde472404dcbe6df6ba3a59a55986
Katherina233 <3083477386@qq.com>
2017-02-26 21:22:59 +0800

    Add files via upload

---
+++ first homework.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+
+public class classProgram {
+	public class MainClass {
+		public void main(String ){
+			
+		}
+	}
+
+	public class LoginForm{
+		private UserDAO dao;
+		public void init(){
+			
+		}
+		public void display(){
+			
+		}
+		public void validate()
+                         {
+			
+		}
+	}
+
+	public class UserDAO{
+		private DBUtil db;
+		public boolean findUser(String userName,String userPassword){
+			return false;
+			
+		}
+	}
+
+	public class DBUtil{
+		public Connection getConnection(){
+			return null;
+			
+		}
+	}
+}
